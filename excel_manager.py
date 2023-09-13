3ecbb8747355b7cd79968bc31a6230e7a69aebe3
Paul_dev <paul.alvarezdev@gmail.com>
2023-09-12 19:08:57 -0500

    Tests Robocorp

--- excel_manager.py
+++ excel_manager.py
@@ -1,5 +1,5 @@
 from RPA.Excel.Files import Files
-from datetime import date
+from datetime import datetime
 from typing import Tuple
 import os
 
@@ -13,7 +13,7 @@ class ExcelManager:
             raise ValueError('The input data variable is empty')
         # Create File name and Path
         output_folder = os.path.join('.', 'output')
-        name = 'Output' + date.today().strftime('_%Y-%m-%d_%H-%M-%S') + '.xlsx'
+        name = 'Output' + datetime.today().strftime('_%Y-%m-%d_%H-%M-%S') + '.xlsx'
         filename = os.path.join(output_folder, name)
         # Create Excel file and store data
         wb = self.excel.create_workbook(filename, sheet_name=f'Fresh News')
